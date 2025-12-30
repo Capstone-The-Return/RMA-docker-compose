ea6a5d670f465240715e40b84d48ab9c3abf7cf9
a.kesisi <esd25018@uom.edu.gr>
2025-12-31 01:42:57 +0200

    Test the VITE_API_URL variable

--- frontend/src/services/employeeTickets.js
+++ frontend/src/services/employeeTickets.js
@@ -1,5 +1,6 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
 export const getAllTickets = async () => {
   const res = await fetch(`${API_URL}/tickets`);
