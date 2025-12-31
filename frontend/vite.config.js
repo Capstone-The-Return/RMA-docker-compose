be4596c8f940655e13690f437b483007d199f3c0
a.kesisi <esd25018@uom.edu.gr>
2025-12-31 02:03:16 +0200

    Fixing local environment hot reload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  watch: { 
-    usePolling: true, 
-    interval: 100 
-  }, 
-  host: "0.0.0.0",
+  server: { 
+    host: "0.0.0.0", 
+    watch: { 
+      usePolling: true, 
+      interval: 100, 
+    }, 
+  },
 })
